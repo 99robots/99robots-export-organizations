24545d3080560396982ff0c043c608fde54939c0
Kyle <kjbenk@gmail.com>
2015-07-13 20:47:01 -0400

    Added Features
    * ADDED: Export organizations by contacts, either all of them or just
    the first one

--- 99robots-export-organizations.php
+++ 99robots-export-organizations.php
@@ -241,36 +241,53 @@ class NNRobots_Export_Organizations {
 
 			foreach ($organizations as $organization) {
 
-			    fputcsv($export_file, array(
-					$organization->ID, 																//'Post ID',
-					date('M d Y', strtotime($organization->post_date) ) , 							//'Published Date',
-					date('M d Y', strtotime($organization->post_modified) ), 						//'Modified Date',
-					$organization->post_title, 														//'Title',
-					self::get_data($organization->ID, 'acronym'),  									//'Acronym',
-					$organization->post_content, 													//'Description',
-					self::get_data($organization->ID, 'notes'), 									//'Notes',
-					self::get_data($organization->ID, 'programs'), 									//'Programs',
-					self::get_data($organization->ID, 'annual_events'),								//'Annual Events',
-					self::get_data($organization->ID, 'postal_address'),							//'Postal Address',
-					self::get_data($organization->ID, 'street'),									//'Street',
-					self::get_data($organization->ID, 'city'),										//'City',
-					self::get_data($organization->ID, 'province'),									//'Province/State',
-					self::get_data($organization->ID, 'postal_code'),								//'Zip Code',
-					self::get_data($organization->ID, 'phone'),										//'Phone',
-					self::get_data($organization->ID, 'fax'),										//'Fax',
-					self::get_data($organization->ID, 'email'),										//'Email',
-					self::get_data($organization->ID, 'website'),									//'Website',
-					self::get_data($organization->ID, 'open_hours'),								//'Open Hours',
-					self::get_data($organization->ID, 'square_footage'),							//'Meeting space',
-					'',//'Contact Name',
-					'',//'Contact Phone',
-					'',//'Contact Email',
-					'',//'Contact Position',
-					self::get_organization_terms($organization->ID, 'org_type'), 					//'Organization Type',
-					self::get_organization_terms($organization->ID, 'post_tag'), 					//'Organization Tags',
-					self::get_parent_categories($organization->ID), 								//'Parent Categories',
-					self::get_child_categories($organization->ID),									//'Child Categories',
-				));
+				$contacts = get_post_meta($organization->ID, 'contacts_multi', true);
+
+				// Loop through all contacts and treat each one as its own entry
+
+				for ($i = 0; $i < $contacts; $i++) {
+
+					$export_array = array(
+						$organization->ID, 																//'Post ID',
+						date('M d Y', strtotime($organization->post_date) ) , 							//'Published Date',
+						date('M d Y', strtotime($organization->post_modified) ), 						//'Modified Date',
+						$organization->post_title, 														//'Title',
+						self::get_data($organization->ID, 'acronym'),  									//'Acronym',
+						$organization->post_content, 													//'Description',
+						self::get_data($organization->ID, 'notes'), 									//'Notes',
+						self::get_data($organization->ID, 'programs'), 									//'Programs',
+						self::get_data($organization->ID, 'annual_events'),								//'Annual Events',
+						self::get_data($organization->ID, 'postal_address'),							//'Postal Address',
+						self::get_data($organization->ID, 'street'),									//'Street',
+						self::get_data($organization->ID, 'city'),										//'City',
+						self::get_data($organization->ID, 'province'),									//'Province/State',
+						self::get_data($organization->ID, 'postal_code'),								//'Zip Code',
+						self::get_data($organization->ID, 'phone'),										//'Phone',
+						self::get_data($organization->ID, 'fax'),										//'Fax',
+						self::get_data($organization->ID, 'email'),										//'Email',
+						self::get_data($organization->ID, 'website'),									//'Website',
+						self::get_data($organization->ID, 'open_hours'),								//'Open Hours',
+						self::get_data($organization->ID, 'square_footage'),							//'Meeting space',
+						self::get_data($organization->ID, 'contacts_multi_' . $i . '_contact_name'),	//'Contact Name',
+						self::get_data($organization->ID, 'contacts_multi_' . $i . '_contact-number'),	//'Contact Phone',
+						self::get_data($organization->ID, 'contacts_multi_' . $i . '_contact-email'),	//'Contact Email',
+						self::get_data($organization->ID, 'contacts_multi_' . $i . '_contact-position'),//'Contact Position',
+						self::get_organization_terms($organization->ID, 'org_type'), 					//'Organization Type',
+						self::get_organization_terms($organization->ID, 'post_tag'), 					//'Organization Tags',
+						self::get_parent_categories($organization->ID), 								//'Parent Categories',
+						self::get_child_categories($organization->ID),									//'Child Categories',
+					);
+
+					// Write to the file
+
+				    fputcsv($export_file, $export_array);
+
+					if ( isset($_GET[self::$prefix_dash . 'organization-contacts']) &&
+						$_GET[self::$prefix_dash . 'organization-contacts'] == 'first_contact' ) {
+
+						break;
+					}
+				}
 			}
 
 			fclose($export_file);
